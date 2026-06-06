5248cb676295a26bb67f158fddabed8a5551bdaa
BP13 Champ <monkzaa007@hotmail.com>
2026-06-06 14:48:34 +0700

    Fin : ex-1 and ex-2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,24 @@
 //Exercise #1: Variable Creation
-//Start coding here
+
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const totalbills = 15000
+const Totalrestaurantfranchises = 1500 
+
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+
+
+const restaurantName = "Chef's Table by Chef James";
+const totalbills = 5000
+const Totalrestaurantfranchises = 2500
+console.log(restaurantName); //Chef's Table by Chef James
+console.log(totalbills); //5000
+console.log(Totalrestaurantfranchises); //2500
